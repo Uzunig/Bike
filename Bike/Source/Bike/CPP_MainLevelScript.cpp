d201b6ddf48360f7a62500e43278fb508dae8a02
Uzunig <Uzunig@gmail.com>
2023-12-19 16:41:32 +0100

    Round moving

--- Bike/Source/Bike/CPP_MainLevelScript.cpp
+++ Bike/Source/Bike/CPP_MainLevelScript.cpp
@@ -6,17 +6,19 @@
 #include "CPP_BigLink.h"
 #include "CPP_SmallLink.h"
 
+
 void ACPP_MainLevelScript::InitLevel()
 {
-    for (int i = 0; i < 360; ++i)
+    for (int i = 0; i < 180; ++i)
     {
-        FVector Location(0.0f, 2000.0f * cos(i * 3.14 / 180), 2000.0f * sin(i * 3.14 / 180));
-        FRotator Rotation(0.0f, 0.0f, -i + 90.0);
+        FVector Location(0.0f, 1300.0f * cos(i * 2 * 3.14 / 180), 1300.0f * sin(i * 2 * 3.14 / 180));
+        FRotator Rotation(0.0f, 0.0f, -i * 2 + 90.0);
         FActorSpawnParameters SpawnInfo;
-        Chain.Add(GetWorld()->SpawnActor<ACPP_BigLink>(Location, Rotation, SpawnInfo));
+        Chain.Add(GetWorld()->SpawnActor<ACPP_SmallLink>(Location, Rotation, SpawnInfo));
         /*
-        Location = FVector(0.0f, 100.0f * cos(i + 0.5), 100.0f * sin(i + 0.5));
-        Chain.Add(GetWorld()->SpawnActor<ACPP_SmallLink>(Location, Rotation, SpawnInfo));*/
+        Location = FVector(0.0f, 1300.0f * cos((i * 2 + 1) * 3.14 / 180), 1300.0f * sin((i * 2 + 1) * 3.14 / 180));
+        Rotation = FRotator(0.0f, 0.0f, (-i * 2 + 1) + 90.0);
+        Chain.Add(GetWorld()->SpawnActor<ACPP_BigLink>(Location, Rotation, SpawnInfo));*/
     }
 }
 
@@ -24,9 +26,14 @@ void ACPP_MainLevelScript::UpdateLevel(float DeltaTime)
 {
     for (int i = 0; i < Chain.Num(); ++i)
     {
-        
-        Chain[i]->SetActorLocation(FVector(0.0f, 2000.0f * cos((i + DeltaTime/1000) * 3.14 / 180), 2000.0f * sin((i + DeltaTime/1000) * 3.14 / 180)));
-        
+        FVector Direction = FVector::CrossProduct(Chain[i]->GetActorLocation(), FVector(1.0f, 0.0f, 0.0f));
+        Direction.Normalize();
+        Chain[i]->SetActorLocation(Chain[i]->GetActorLocation() + Direction * DeltaTime * 100);
+        FVector Cross = FVector::CrossProduct(Chain[i]->GetActorLocation(), FVector(1.0f, 0.0f, 0.0f));
+        Cross.Normalize();
+        FRotator DeltaRotation = FRotator(0.0f, 0.0f, (acos(FVector::DotProduct(Direction, Cross))) * 180 / 3.14);
+        Chain[i]->SetActorRelativeRotation(Chain[i]->GetActorRotation() + DeltaRotation);
+     
     }
 }
 
